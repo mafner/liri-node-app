bcc3d2dcb6c391a7252996326976b77739204474
Matt Hefner <matt.r.hefner@gmail.com>
2019-08-20 12:00:58 -0700

    spotify keys added

--- liri.js
+++ liri.js
@@ -1,5 +1,13 @@
-require("dotenv").config();
-
+// libs required:
+var request = require('request');
+var fs = require('fs');
+var dotenv = require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
 
+//moment (date configuration):
+var moment = require('moment');
+moment().format();
+
+//spotify keys (constructor)
 var spotify = new Spotify(keys.spotify);
